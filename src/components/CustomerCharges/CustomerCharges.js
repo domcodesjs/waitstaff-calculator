63e042e713fc54de964f49629886363bbe944cfd
Dominic Faulring <domcodesjs@gmail.com>
2020-10-15 11:58:57 -0700

    fixed a typo

--- src/components/CustomerCharges/CustomerCharges.js
+++ src/components/CustomerCharges/CustomerCharges.js
@@ -4,7 +4,7 @@ import styled from 'styled-components';
 const CustomerCharges = ({ baseMealPrice, tipPercentage }) => {
   return (
     <StyledSection>
-      <h2>Customer: Charges</h2>
+      <h2>Customer Charges</h2>
       <p>Subtotal: ${baseMealPrice}</p>
       <p>Tip: ${baseMealPrice * (tipPercentage / 100)}</p>
       <p>
